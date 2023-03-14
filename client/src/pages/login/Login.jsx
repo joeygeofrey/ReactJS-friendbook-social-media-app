c4d11adf11002915bb3eb71b42584f45534e08d4
Joey <joeygeof.dev@gmail.com>
2023-03-14 10:30:57 -0400

    Update: login page - add text, form, buttons

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,7 +2,26 @@ import "./login.scss"
 
 const Login = () => {
     return (
-        <div className="login">Login</div>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World.</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque a numquam, praesentium quos fuga qui repellendus, ab amet, iure quidem dignissimos enim distinctio molestias! Autem dolor totam iusto voluptas debitis!
+                    </p>
+                    <span>Don't have an account?</span>
+                    <button>Register</button>
+                </div>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
