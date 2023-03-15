7bdb13bc8149d12d6131fa0adc6628a4f0efb24d
Joey <joeygeof.dev@gmail.com>
2023-03-14 22:55:07 -0400

    Update: contents for navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,49 +1,35 @@
 import "./navbar.scss";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
-import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { DarkModeContext } from "../../context/darkModeContext";
-import { AuthContext } from "../../context/authContext";
 
 const Navbar = () => {
-  const { toggle, darkMode } = useContext(DarkModeContext);
-  const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="navbar">
       <div className="left">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span>lamasocial</span>
+        <Link to="/" style={({textDecoration: "none"})} >
+          <span>friendbook</span>
         </Link>
-        <HomeOutlinedIcon />
-        {darkMode ? (
-          <WbSunnyOutlinedIcon onClick={toggle} />
-        ) : (
-          <DarkModeOutlinedIcon onClick={toggle} />
-        )}
-        <GridViewOutlinedIcon />
+        <HomeRoundedIcon/>
+        <DarkModeOutlinedIcon/>
+        <WidgetsOutlinedIcon/>
         <div className="search">
-          <SearchOutlinedIcon />
-          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon/>
+          <input type="text" placeholder="search"/>        
         </div>
       </div>
       <div className="right">
-        <PersonOutlinedIcon />
-        <EmailOutlinedIcon />
-        <NotificationsOutlinedIcon />
+        <PersonOutlinedIcon/>
+        <EmailRoundedIcon/>
+        <NotificationsOutlinedIcon/>
         <div className="user">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <span>{currentUser.name}</span>
+          <span>Joey Geofrey</span>
         </div>
       </div>
     </div>
