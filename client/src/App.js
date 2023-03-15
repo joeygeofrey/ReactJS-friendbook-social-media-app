0367edd1de6f8f3278ca90f41e062c1293c89387
Joey <joeygeof.dev@gmail.com>
2023-03-14 22:24:46 -0400

    Update: add react router and helper functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,78 @@
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Leftbar from "./components/leftBar/LeftBar";
+import Rightbar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+
+  const currentUser = true;
+
+  const Layout = () => {    
+    return (
+      <div>
+        <Navbar />
+        <div style={{display: "flex" }}>
+          <Leftbar />
+          <Outlet />
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children}) => {
+    if(!currentUser) {
+      return <Navigate to="/login"/>
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+           <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
+
+
   return (
     <div>
-      <Login/>
+      <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
