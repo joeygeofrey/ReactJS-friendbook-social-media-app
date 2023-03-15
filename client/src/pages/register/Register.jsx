89e55fcd0ee3e74acee09d4acc71b7e160fdcb86
Joey <joeygeof.dev@gmail.com>
2023-03-14 22:54:06 -0400

    Update: content for register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom"
 import "./register.scss"
 
 const Register = () => {
     return (
-        <div className="register">Register</div>
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <h1>let's go!</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque a numquam, praesentium quos fuga qui repellendus, ab amet, iure quidem dignissimos enim distinctio molestias! Autem dolor totam iusto voluptas debitis!
+                    </p>
+                    <span>Do you have an account?</span>
+                    <Link to="/login">
+                    <button>Login</button>
+                    </Link>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
