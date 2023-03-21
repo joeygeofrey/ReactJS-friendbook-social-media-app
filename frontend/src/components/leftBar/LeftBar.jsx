dcdb11c768fcdbc426a2b1bea8470e2b4946c64a
Joey <joeygeof.dev@gmail.com>
2023-03-20 23:19:09 -0400

    Update: content

--- frontend/src/components/leftBar/LeftBar.jsx
+++ frontend/src/components/leftBar/LeftBar.jsx
@@ -1,17 +1,4 @@
 import "./leftbar.scss";
-import Friends from "../../assets/1.png";
-import Groups from "../../assets/2.png";
-import Market from "../../assets/3.png";
-import Watch from "../../assets/4.png";
-import Memories from "../../assets/5.png";
-import Events from "../../assets/6.png";
-import Gaming from "../../assets/7.png";
-import Gallery from "../../assets/8.png";
-import Videos from "../../assets/9.png";
-import Messages from "../../assets/10.png";
-import Tutorials from "../../assets/11.png";
-import Courses from "../../assets/12.png";
-import Fund from "../../assets/13.png";
 
 const LeftBar = () => {
 
@@ -32,16 +19,11 @@ const LeftBar = () => {
             <span>Circles</span>
           </div>
           <div className="item">
-            <img src="https://cdn-icons-png.flaticon.com/512/3592/3592265.png" alt="" />
-            <span>Events</span>
-          </div>
-          <div className="item">
             <img src="https://cdn-icons-png.flaticon.com/512/4322/4322936.png" alt="" />
             <span>Memories</span>
           </div>
           <hr/>
           <div className="menu">
-            <span>Your shortcuts</span>
           </div>
           <div className="item">
             <img src="https://cdn-icons-png.flaticon.com/512/4240/4240459.png" alt="" />
@@ -61,23 +43,18 @@ const LeftBar = () => {
           </div>
           <hr/>
           <div className="menu">
-            <span>Others</span>
           </div>
           <div className="item">
             <img src="https://cdn-icons-png.flaticon.com/512/3592/3592017.png" alt="" />
             <span>Marketplace</span>
           </div>
           <div className="item">
-            <img src={Tutorials} alt="" />
-            <span>Tutorials</span>
+            <img src="https://cdn-icons-png.flaticon.com/512/3592/3592265.png" alt="" />
+            <span>Events</span>
           </div>
           <div className="item">
-            <img src={Courses} alt="" />
-            <span>Friends</span>
-          </div>
-          <div className="item">
-            <img src={Fund} alt="" />
-            <span>Fund</span>
+            <img src="https://cdn-icons-png.flaticon.com/512/4293/4293599.png" alt="" />
+            <span>Give</span>
           </div>
         </div>
       </div>
