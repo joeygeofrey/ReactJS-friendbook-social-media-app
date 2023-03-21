0878b1e3179125c6354cbe0a68b2f506c53293f1
Joey <joeygeof.dev@gmail.com>
2023-03-20 22:52:01 -0400

    Update: stories media and text content

--- frontend/src/components/stories/Stories.jsx
+++ frontend/src/components/stories/Stories.jsx
@@ -4,43 +4,47 @@ import { AuthContext } from "../../context/authContext"
 
 const Stories = () => {
 
-  const {currentUser} = useContext(AuthContext)
-
-  //TEMPORARY
   const stories = [
     {
       id: 1,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Joey Geofrey",
+      img: "https://www.whitescreen.online/image/black-background.png",
+      button: "+"
     },
     {
       id: 2,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Maria Torres",
+      img: "https://images.pexels.com/photos/1839238/pexels-photo-1839238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 3,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Blake Hopkins",
+      img: "https://images.pexels.com/photos/8448073/pexels-photo-8448073.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       id: 4,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Carlos Rodriguez",
+      img: "https://images.pexels.com/photos/3879058/pexels-photo-3879058.jpeg",
+    },
+    {
+      id: 5,
+      name: "Aisha Rahman",
+      img: "https://images.pexels.com/photos/2884866/pexels-photo-2884866.jpeg",
+    },
+    {
+      id: 6,
+      name: "Jamal Ahmed",
+      img: "https://images.pexels.com/photos/3884710/pexels-photo-3884710.jpeg",
     },
   ];
 
   return (
     <div className="stories">
-      <div className="story">
-          <img src={currentUser.profilePic} alt="" />
-          <span>{currentUser.name}</span>
-          <button>+</button>
-        </div>
-      {stories.map(story=>(
-        <div className="story" key={story.id}>
+      {stories.map(story => (
+        <div className="story">
           <img src={story.img} alt="" />
           <span>{story.name}</span>
+          <button>{story.button}</button>
         </div>
       ))}
     </div>
